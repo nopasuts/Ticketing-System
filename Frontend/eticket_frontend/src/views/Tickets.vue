85e6532e8de62cdf850ac63baf88c730660822dd
Atitaya <fang.atit2497@gmail.com>
2020-02-14 23:45:41 +0700

    Add table to Tickets.vue

--- Frontend/eticket_frontend/src/views/Tickets.vue
+++ Frontend/eticket_frontend/src/views/Tickets.vue
@@ -3,12 +3,15 @@
     <NavBar/>
     <b-container fluid>
       <b-row>
-        <b-col class="ticketBox" cols="2">
-          <h3>Tickets</h3>
+        <b-col class="ticketBox" cols="3">
+          <h3>Ticket Categories</h3>
           <br/>
           All tickets : <br/>
           Open tickets :<br/>
           Unassigned ticket :<br/>
+        </b-col>
+        <b-col class="ticketBox" cols="8">
+          <b-table striped hover :items="items" :fields="fields"></b-table>
         </b-col>
       </b-row>
     </b-container>
@@ -21,6 +24,43 @@ export default {
   name: 'tickets',
   components: {
     NavBar
+  },
+  data(){
+    return{
+      fields: [
+          {
+            key: 'Ticket',
+            sortable: true
+          },
+          {
+            key: 'Status',
+            sortable: false
+          },
+          {
+            key: 'Priority',
+            label: 'Priority',
+            sortable: true,
+            // Variant applies to the whole column, including the header and footer
+            variant: 'danger'
+          },
+          {
+            key: 'Date',
+            sortable: false
+          },
+          {
+            key: 'Due',
+            sortable: false
+          },
+          {
+            key: 'Reply',
+            sortable: true
+          }
+        ],
+      items: [
+          { Ticket:"Cannot do this" , Status:"Open" , Priority:"Normal" ,Date:"01-01-2020",Due:"02-01-2020",Reply:2 },
+          { Ticket:"Cannot do that" , Status:"Open" , Priority:"Normal" ,Date:"01-01-2020",Due:"02-01-2020",Reply:1 }
+        ]
+    }
   }
 }
 </script>
