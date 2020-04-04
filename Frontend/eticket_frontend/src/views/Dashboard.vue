2d0e997bbbbd9f5cdf7b383e77443f9fbabd88d7
Nopasut <ampamp2541@hotmail.com>
2020-04-04 23:13:23 +0700

    Add admin navbar

--- Frontend/eticket_frontend/src/views/Dashboard.vue
+++ Frontend/eticket_frontend/src/views/Dashboard.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
+        <AdminNavbar/>
         <p>This is dashboard</p>
     </div>
 </template>
+<script>
+import AdminNavbar from '@/components/AdminNavBar'
+export default {
+    components:{
+        AdminNavbar
+    }
+}
+</script>
